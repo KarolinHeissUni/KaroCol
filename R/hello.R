bd18a7d66e7b1238c1208d489c871d37d87463f7
Karolin Heiss <karolin.heiss@gmail.com>
2023-05-15 18:24:50 +0200

    v0.1.3

--- R/hello.R
+++ R/hello.R
@@ -13,6 +13,28 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+
+
+col_karo_1 <- "#d13896"
+col_karo_2 <- "#849895"
+col_karo_3 <- "#4c3575"
+col_karo_4 <- "#ffa252"
+col_karo_5 <- "#006b6b"
+
+uni_karo_1 <- unikn::newpal(col = c(col_karo_1, col_karo_2, col_karo_3, col_karo_4, col_karo_5),
+                     names = c("uni karo 1", "uni karo 2", "uni karo 3", "uni karo 4", "uni karo 5"),
+                     as_df = FALSE)
+
+#' Show new Color pal "uni_karo_1"
+#' @export
+showKaroColors <- function() {
+  unikn::seecol(uni_karo_1, main = "Primary colors for KARO")
+}
+
+
+
+
+
 #' Prints "Hello Karo!" text
 #' @export
 hello <- function() {
